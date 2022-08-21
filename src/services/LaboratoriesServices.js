66b33754f948459ac15b868a500e826f643fc64d
Rodrigo Acosta Cruz <rodrigoacosta1115@gmail.com>
2022-08-20 23:10:00 -0300

    fixed laboratories service

--- src/services/LaboratoriesServices.js
+++ src/services/LaboratoriesServices.js
@@ -3,7 +3,7 @@ import api from "../api/axios";
 const API_BASE_URL = import.meta.env.VITE_URL_BASE;
 
 export const getLaboratories = (show = 10, search = null, page = 1) => {
-  const url = API_BASE_URL + `/laboratories?limit=${show}&from=${page}`;
+  let url = API_BASE_URL + `/laboratories?limit=${show}&from=${page}`;
 
   if (search) {
     url += `&search=${search}`
